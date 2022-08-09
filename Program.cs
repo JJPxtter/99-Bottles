68b7f78aece255fe52a0763434a726623039c7b1
Jeandre Putter <81087421+JJPxtter@users.noreply.github.com>
2022-08-09 13:34:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 99;
+
+while (i != 1)
+{
+    string bottles = i.ToString();
+    Console.WriteLine("There's "+bottles+" bottles of beer on the wall");
+    i = i - 1;
+    Console.WriteLine("Take one down,");
+}
+
+if (i == 1)
+{
+    string bottle = i.ToString();
+    Console.WriteLine("There's " + bottle + " bottle of beer on the wall");
+}
